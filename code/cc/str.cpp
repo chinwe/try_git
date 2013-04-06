07b3947dcda329ee713130ba1fa6d3ff7c2518e0
chinwe <chinwe122@gmail.com>
2013-04-06 15:23:20 +0800

    add cc floder

---
+++ code/cc/str.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string str("hello, github");
+	cout << str << endl;
+	cout << str.substr(7, 13) << endl;
+	return 0;
+}
